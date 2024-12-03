7bd62756ca69f169ff9bf2cd65f562b48ce3da5c
wazcoder <wilmaralexanderzapata@outlook.com>
2024-12-03 16:44:23 -0500

    taller tps

---
+++ pasado.py
@@ -0,0 +1,7 @@
+# ficha TPS 3059802
+
+edad = int(input("tu edad: "))
+if edad >= 18:
+    print("eres mayor de edad puedes ingresar")
+elif edad <=18:
+    print("eres menor de edad NO puedes ingresar")
